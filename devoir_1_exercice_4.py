a0144bf4ddb76730bb66fa7eb8e554d9dce7e7a9
ScientificWare <info@scientificware.com>
2019-01-22 23:43:36 +0100

    Commit initial
    Programme affichant les nombres d'Armstrong compris entre 0 et 999.

---
+++ devoir_1_exercice_4.py
@@ -0,0 +1,8 @@
+﻿# Liste des nombres d'Armstrong compris entre 0 et 999
+print("Liste des nombres d'Armstrong compris entre 0 et 999")
+for c in range(0,9,1) :
+    for d in range(0,9,1) :
+        for u in range(0,9,1) :
+            n = c*100+d*10+u
+            if n == c**3 + d**3 + u**3 :
+                print(n,"=",c,"³+",d,"³+",u,"³")
