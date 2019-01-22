5cd0c56712b762d7863cbc7fdf24ffd93fa6be4e
ScientificWare <info@scientificware.com>
2019-01-22 22:01:59 +0100

    Changement de l'ordre des tests.
    Changement de l'ordre des tests.
    Évite de tester les cas d'initialisation des deux premiers termes lors des calculs des termes de rangs supérieurs à 1.

--- devoir_1_exercice_1.py
+++ devoir_1_exercice_1.py
@@ -1,16 +1,21 @@
-﻿un=0
+# Affichage des termes strictement inférieurs à 1000 de la suite numérique uₙ=5×Uₙ₋₁-2×Uₙ₋₂.
+# Initialisation des variables.
+un=0
 n=0
 while un < 1000 :
-    if n == 0 :
-        un = 2
+    if n > 1 :
+	# Met à jour les termes de rang n-1 et n-2
+        un2 = un1
+        un1 = un
+        un = 5 * un1 - 2 * un2
     else :
-       if n == 1 :
-           un1 = un
-           un = 1
-       else :
-           un2 = un1
-           un1 = un
-           un = 5 * un1 - 2 * un2
+	# Initialise le premier terme.
+        if n == 0 :
+            un = 2
+        else :
+            # Initialise les deux premiers termes.
+            if n == 1 :
+                un1 = un
+                un = 1
     print ("U(",n,")=",un)
     n = n + 1
-
