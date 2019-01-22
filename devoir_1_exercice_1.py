b96cb1f8d0effa8e55ad775a27d2af770966c2e7
ScientificWare <info@scientificware.com>
2019-01-22 21:34:49 +0100

    Commit initial

---
+++ devoir_1_exercice_1.py
@@ -0,0 +1,16 @@
+﻿un=0
+n=0
+while un < 1000 :
+    if n == 0 :
+        un = 2
+    else :
+       if n == 1 :
+           un1 = un
+           un = 1
+       else :
+           un2 = un1
+           un1 = un
+           un = 5 * un1 - 2 * un2
+    print ("U(",n,")=",un)
+    n = n + 1
+
