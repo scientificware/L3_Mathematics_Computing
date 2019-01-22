8fad9871a7fde829a81cc4c98a4bf4960bffe3f4
ScientificWare <info@scientificware.com>
2019-01-22 17:56:12 +0100

    Commit initial

---
+++ tp_1_exercice_1.py
@@ -0,0 +1,20 @@
+﻿x=int(input("Entrer le premier opérande : "))
+y=int(input("Entrer le second opérande : "))
+
+resultat = x + y
+print ( x , "+" , y , "=" , resultat)
+
+resultat = x - y
+print ( x , "-" , y , "=", resultat)
+
+resultat = x*y
+print ( x , "×" , y , "=", resultat)
+
+if y==0 :
+    if x==0 :
+        print ("L'opération 0 ÷ 0 donne une infinité de valeurs")
+    else :
+        print ("L'opération",x,"÷ 0 n'est pas définie")
+else :
+    resultat = x/y
+    print ( x, "÷" , y , "=" , resultat)
