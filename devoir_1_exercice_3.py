e25892195a572fdd38c9f5ed6b35859f7901c2a8
ScientificWare <info@scientificware.com>
2019-01-24 00:03:15 +0100

    Commit initial
    Exercice 3 du devoir 1 : Programmation du crible d'Eratosthène.

---
+++ devoir_1_exercice_3.py
@@ -0,0 +1,34 @@
+﻿# Crible d'Eratosthène.
+
+# Saisie de la valeur maximale des nombres premiers supérieurs à 2.
+n = 0
+while n <2 :
+    n = int(input("Valeur maximale des nombres premiers supérieurs à 2, n = "))
+
+# Création de la liste des nombres compris entre 2 et n.
+l = [i for i in range(2,n+1)]
+
+# Suppression des multiples de chaque nombre compris dans la première moitié de la liste.
+print("Liste des valeurs supprimées à chaque itération :")
+for i in range(2,n//2+1) :
+    s = 2*i
+    print("sup", end=" ")
+    while s <= n :
+        print(s, end=" ")
+        l[s-2] = 'k'
+        s = s + i
+    print()
+
+# Suppression des blancs.
+for k in range(1,l.count('k')+1) :
+        l.remove('k')
+
+# Affichage des nombres premiers par dizaine.
+print("Liste, par dizaine, des nombres premiers compris entre 1 et",n)
+compt = 11
+for i in range(len(l)) :
+    # Retour à la ligne à chaque début d'une dizaine.
+    if compt <= l[i] :
+        print()
+        compt = (l[i]//10 + 1)*10
+    print(l[i], end=" ")
