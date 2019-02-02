49cff8870ae751bd7baf6fc658bf77ad05c82e62
ScientificWare <info@scientificware.com>
2019-02-02 16:22:41 +0100

    Optimisation du code.
    Réduction du nombre de recherche de multiples.
    Correction de la plage de recherche.

--- devoir_1_exercice_3.py
+++ devoir_1_exercice_3.py
@@ -1,4 +1,4 @@
-﻿# Crible d'Eratosthène.
+# Crible d'Eratosthène.
 
 # Saisie de la valeur maximale des nombres premiers supérieurs à 2.
 n = 0
@@ -10,18 +10,18 @@ l = [i for i in range(2,n+1)]
 
 # Suppression des multiples de chaque nombre compris dans la première moitié de la liste.
 print("Liste des valeurs supprimées à chaque itération :")
-for i in range(2,n//2+1) :
+for i in range(2,int(n**0.5)+1) :
     s = 2*i
     print("sup", end=" ")
     while s <= n :
         print(s, end=" ")
-        l[s-2] = 'k'
+        l[s-2] = ' '
         s = s + i
     print()
 
 # Suppression des blancs.
-for k in range(1,l.count('k')+1) :
-        l.remove('k')
+for k in range(1,l.count(' ')+1) :
+        l.remove(' ')
 
 # Affichage des nombres premiers par dizaine.
 print("Liste, par dizaine, des nombres premiers compris entre 1 et",n)
